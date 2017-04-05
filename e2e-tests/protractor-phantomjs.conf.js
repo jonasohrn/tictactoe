43c134824858c975e95fbbfb8e705e7b05a71517
jonas.ohrn <jonas.ohrn@maxm.se>
2017-04-05 23:55:53 +0200

    Updated Karma and added PhantomJS

---
+++ e2e-tests/protractor-phantomjs.conf.js
@@ -0,0 +1,35 @@
+exports.config = {
+  allScriptsTimeout: 4000,
+
+  specs: [
+    '*.js'
+  ],
+
+  capabilities: {
+  //  'browserName': 'chrome'
+
+ 'browserName': 'phantomjs',
+
+  /* 
+   * Can be used to specify the phantomjs binary path.
+   * This can generally be ommitted if you installed phantomjs globally.
+   */
+  'phantomjs.binary.path': require('phantomjs-prebuilt').path,
+
+  /*
+   * Command line args to pass to ghostdriver, phantomjs's browser driver.
+   * See https://github.com/detro/ghostdriver#faq
+   */
+  'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
+
+  },
+
+
+  baseUrl: 'http://localhost:8000/app/',
+
+  framework: 'jasmine',
+
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 5000
+  }
+};
